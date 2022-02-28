120299a2e68980f05106c052182729e306e4a533
Xuan Chen <xchen586@gmail.com>
2022-02-27 23:42:41 -0500

    Change library path.

--- TSVBMacTest/MCVBReplacer.mm
+++ TSVBMacTest/MCVBReplacer.mm
@@ -44,54 +44,86 @@ using namespace manycam;
     NSError *error;
     if (![[NSFileManager defaultManager] fileExistsAtPath:mcvbNSPath]) {
         [[NSFileManager defaultManager] createDirectoryAtPath:mcvbNSPath withIntermediateDirectories:NO attributes:nil error:&error]; //Create folder
+        NSLog(@"Library is not exist!");
         return NO;
     }
     
     const char * path = NULL;
     //path = "libmcvb.dylib";
     //path = [mcvbNSPath UTF8String];
-    path = [mcvbNSPath cStringUsingEncoding:NSASCIIStringEncoding];
-    //path = [mcvbNSPath cStringUsingEncoding:NSUTF8StringEncoding];
+    //path = [mcvbNSPath cStringUsingEncoding:NSASCIIStringEncoding];
+    path = [mcvbNSPath cStringUsingEncoding:NSUTF8StringEncoding];
     
     if (!path) {
+        NSLog(@"Can not get library char path.");
         return NO;
     }
     
-    void * handle = dlopen(path, RTLD_LOCAL);
+    void * handle = dlopen(path, RTLD_NOW);
     if (!handle) {
+        char * error = dlerror();
+        NSLog(@"dlopen error: %s", error);
         return NO;
     }
    
     _createFactory = reinterpret_cast<manycam::pfnCreateSDKFactory>(dlsym(handle, "createSDKFactory"));
     if (!_createFactory) {
+        NSLog(@"Fail to get function pointer of createSDKFactory");
         return NO;
     }
     
     _sdkFactory = _createFactory();
-    if (_sdkFactory) {
+    if (!_sdkFactory) {
+        NSLog(@"Fail to createFactory!");
         return NO;
     }
-    return NO;
+    
+    _pipeline = _sdkFactory->createPipeline();
+    if (!_pipeline) {
+        NSLog(@"Fail to create pipeline!");
+        return NO;
+    }
+    
+    _frameFactory = _sdkFactory->createFrameFactory();
+    if (!_frameFactory) {
+        NSLog(@"Fail to createFrameFactory!");
+        return NO;
+    }
+    
+    return YES;
 }
 
 - (NSString *)getMCVBNSPath {
+    //return [self getMCVBNSPathFromFrameWork];
+    return [self getMCVBNSPathFromResource];
+}
+
+- (NSString *)getMCVBNSPathFromFrameWork {
     NSString *dataPath;
+    
 #if 0
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0]; // Get documents folder
-
     dataPath = [documentsDirectory stringByAppendingPathComponent:@"libmcvb.dylib"];
 #endif
     
     NSBundle* bundle = [NSBundle mainBundle];
+#if 0
     NSArray<NSBundle *> *allFrameworks = [NSBundle allFrameworks];
     NSArray<NSBundle *> *allBundles = [NSBundle allBundles];
-    dataPath = [bundle pathForResource:@"libmcvb" ofType:@"dylib"];
     NSURL * sharedFrameWorkUrl = bundle.sharedFrameworksURL;
     NSString * sharedFrameworkPath = bundle.sharedFrameworksPath;
     NSString * execPath = bundle.executablePath;
+#endif
     NSString * privateFrameworkPath = bundle.privateFrameworksPath;
     dataPath = [privateFrameworkPath stringByAppendingPathComponent:@"libmcvb.dylib"];
+    return dataPath;
+}
+
+- (NSString *)getMCVBNSPathFromResource {
+    NSString * dataPath;
+    NSBundle* bundle = [NSBundle mainBundle];
+    dataPath = [bundle pathForResource:@"libmcvb" ofType:@"dylib"];
     return dataPath;
 }
 
